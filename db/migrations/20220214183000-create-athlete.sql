48fbdff9966d35b3359c2d24f3ae473f72ea574c
yvancubahiro <yvancubahiro@gmail.com>
2022-02-28 11:25:33 -0500

    added migrations

---
+++ db/migrations/20220214183000-create-athlete.sql
@@ -0,0 +1,7 @@
+CREATE TABLE athletes(
+    id int,
+    name_athlete varchar(20),
+    identified_gender varchar(1),
+    dob date
+)
+
---
+++ db/migrations/20220214192000-create-migrations.sql
@@ -0,0 +1,5 @@
+CREATE TABLE schema_migrations (
+ migration varchar(255),
+ migrated_at time,
+ PRIMARY KEY (migration)
+);
